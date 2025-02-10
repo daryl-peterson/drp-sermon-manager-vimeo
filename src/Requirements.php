9e800b49c8c665f23f44ab57185667f7a7580045
daryl-peterson <daryl.peterson@gmail.com>
2025-02-10 08:28:06 -0600

    Refactoring

--- src/Requirements.php
+++ src/Requirements.php
@@ -12,73 +12,70 @@ use DRPSMVimeo\Core\Interfaces\RequirementsInterface;
  * @copyright   Copyright (c) 2024, Daryl Peterson
  * @license     https://www.gnu.org/licenses/gpl-3.0.txt
  */
-class Requirements implements RequirementsInterface
-{
-    private NoticeInterface $notice;
+class Requirements implements RequirementsInterface {
 
-    public function __construct(NoticeInterface $notice)
-    {
-        $this->notice = $notice;
-    }
+	private NoticeInterface $notice;
 
-    public function init(): void
-    {
-        try {
-            $hook = Helper::getKeyName('REQUIREMENTS_INIT');
+	public function __construct( NoticeInterface $notice ) {
+		$this->notice = $notice;
+	}
 
-            if (did_action($hook) && !defined('PHPUNIT_TESTING')) {
-                // @codeCoverageIgnoreStart
-                return;
-                // @codeCoverageIgnoreEnd
-            }
-            add_action('admin_init', [$this, 'isCompatible']);
-            Logger::debug('REQUIREMENTS HOOKS INITIALIZED');
-            do_action($hook);
+	public function init(): void {
+		try {
+			$hook = Helper::getKeyName( 'REQUIREMENTS_INIT' );
 
-            // @codeCoverageIgnoreStart
-        } catch (\Throwable $th) {
-            Logger::error(['MESSAGE' => $th->getMessage(), 'TRACE' => $th->getTrace()]);
-            // @codeCoverageIgnoreEnd
-        }
-    }
+			if ( did_action( $hook ) && ! defined( 'PHPUNIT_TESTING' ) ) {
+				// @codeCoverageIgnoreStart
+				return;
+				// @codeCoverageIgnoreEnd
+			}
+			add_action( 'admin_init', array( $this, 'isCompatible' ) );
+			Logger::debug( 'REQUIREMENTS HOOKS INITIALIZED' );
+			do_action( $hook );
 
-    public function notice(): NoticeInterface
-    {
-        return $this->notice;
-    }
+			// @codeCoverageIgnoreStart
+		} catch ( \Throwable $th ) {
+			Logger::error(
+				array(
+					'MESSAGE' => $th->getMessage(),
+					'TRACE'   => $th->getTrace(),
+				)
+			);
+			// @codeCoverageIgnoreEnd
+		}
+	}
 
-    public function isCompatible(): void
-    {
-        $transient = Helper::getKeyName('compatible');
-        try {
-            Logger::debug('CHECKING REQUIREMENTS');
-            $obj = new RequirementChecks();
-            $obj->run();
-            Logger::debug('REQUIREMENTS MET');
+	public function notice(): NoticeInterface {
+		return $this->notice;
+	}
 
-            set_transient($transient, true, 500);
-            // @codeCoverageIgnoreStart
-        } catch (\Throwable $th) {
-            delete_transient($transient);
-            $this->deactivate();
+	public function isCompatible(): void {
+		$transient = Helper::getKeyName( 'compatible' );
+		try {
+			Logger::debug( 'CHECKING REQUIREMENTS' );
+			$obj = new RequirementChecks();
+			$obj->run();
+			Logger::debug( 'REQUIREMENTS MET' );
 
-            return;
-        }
-        // @codeCoverageIgnoreEnd
-    }
+			// @codeCoverageIgnoreStart
+		} catch ( \Throwable $th ) {
+			$this->deactivate();
+			return;
+		}
+		// @codeCoverageIgnoreEnd
+	}
 
-    /**
-     * Deactivate plugin.
-     */
-    private function deactivate(): void
-    {
-        if (is_admin() && current_user_can('activate_plugins')) {
-            deactivate_plugins(FILE);
-            // @codeCoverageIgnoreStart
-            if (isset($_GET['activate'])) {
-                unset($_GET['activate']);
-            }
-            // @codeCoverageIgnoreEnd
-        }
-    }
+	/**
+	 * Deactivate plugin.
+	 */
+	private function deactivate(): void {
+		if ( is_admin() && current_user_can( 'activate_plugins' ) ) {
+			deactivate_plugins( FILE );
+			// @codeCoverageIgnoreStart
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+			// @codeCoverageIgnoreEnd
+		}
+	}
 }
